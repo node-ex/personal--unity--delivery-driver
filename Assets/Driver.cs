2f830a92512526ffaf0657efc2f56b9c144c8716
Pavel Balashov <pavel.balashov1@gmail.com>
2025-01-31 16:26:06 +0100

    feat: rotate and translate the car each frame

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -5,12 +5,13 @@ public class Driver : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        Debug.Log("Hello World!");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        transform.Rotate(0, 0, 0.1f);
+        transform.Translate(0, 0.005f, 0);
     }
 }
